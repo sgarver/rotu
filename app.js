ebf8389576708f315a972ec2801c63eb5c1e3860
Stephen Garver <sgarver@protonmail.ch>
2015-11-10 07:53:15 -0800

    Update to path handling

--- app.js
+++ app.js
@@ -9,33 +9,42 @@ const pageData = require('./pageData.json');
 const server = http.createServer(function (request, response) {
 
     var path = url.parse(request.url).pathname;
-    const options = {};
+    var options = {};
+    var html = '';
 
     response.writeHead(200, {"Content-Type": "text/html"});
 
     if (path === '/favicon.ico') {
+
         response.writeHead(200, {'Content-Type': 'image/x-icon'} );
         response.end();
+
     } else {
 
         try {
 
-            if (path === '/') {
-                path = "/home"
+            if (path[path.length - 1] === '/') {
+                path += "home"
             }
 
-            const template = jade.compileFile('./pages/' + path + '.jade', options);
-            var html = template(pageData[path.replace(/\//g, '')].locals);
+            const template = jade.compileFile('./pages' + path + '.jade', options);
+            html = template(pageData[path.replace(/\//g, '')].locals);
+
         } catch (e) {
+
             html = "404 File Not Found"
             console.log(e);
+
         } finally {
+
             response.writeHead(200, {"Content-Type": "text/html"});
             response.end(html);
             console.log('page served');
+
         }
     }
 });
 
 server.listen(8000);
-console.log("Server running at http://127.0.0.1:8000/");
+console.log("Server running on port 8000");
+
