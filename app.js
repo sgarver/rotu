f86f3970af12e1e75b1ee6bb8ce7dbbfda1ef2f3
Stephen Garver <sgarver@protonmail.ch>
2015-11-11 22:36:29 -0800

    Move Jade template building to module and add error callback

--- app.js
+++ app.js
@@ -1,48 +1,31 @@
 
 const http = require('http');
-const path = require('path');
-const jade = require('jade');
 
 const rotu = require('./rotu');
 const data = require('./data.json');
 
 const server = http.createServer(function (request, response) {
 
-    var html = "";
-    var template = "";
-    var routed = rotu(request.url);
-
-    console.log('rotu -> ' + routed);
-
-    if (routed === '/favicon.ico') {
+    if (request.url === '/favicon.ico') {
 
         response.writeHead(200, {'Content-Type': 'image/x-icon'} );
         response.end();
 
     } else {
 
-        try {
+        response.writeHead(200, {"Content-Type": "text/html"});
 
-            template = jade.compileFile('./pages' + routed + '.jade', { "pretty": true });
-            html = template(data[path.basename(routed)].locals);
-
-        } catch(e) {
-
-            response.writeHead(200, {"Content-Type": "text/html"});
+        var html = rotu(request.url, data, {"pretty": true}, function(e) {
 
             if (e.errno === 34) {
-                response.end('The page ' + routed + ' was not found.');
+                response.end('This page was not found.');
             } else {
-                response.end('There was a problem processing template: ' + routed);
+                response.end('There was a problem processing your template');
                 console.log(e);
             }
+        });
 
-        } finally {
-
-            response.writeHead(200, {"Content-Type": "text/html"});
-            response.end(html);
-
-        }
+        response.end(html);
     }
 });
 
--- rotu.js
+++ rotu.js
@@ -1,14 +1,25 @@
 
 const path = require('path');
 const url = require('url');
+const jade = require('jade');
 
-module.exports = function(route) {
+module.exports = function(route, data, options, err) {
 
-    var routed = path.normalize(url.parse(route).pathname);
+    try {
 
-    if (routed[routed.length - 1] === '/') {
-        routed += "index";
+        var routed = path.normalize(url.parse(route).pathname);
+
+        if (routed[routed.length - 1] === '/') {
+            routed += "index";
+        }
+
+        options = options || {};
+
+        var template = jade.compileFile('./pages' + routed + '.jade', options);
+
+        return template(data[path.basename(routed)].locals);
+
+    } catch(e) {
+        typeof err === 'function' && err(e);
     }
-
-    return routed;
 };
