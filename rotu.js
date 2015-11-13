061c8dd652951abbfd6c95ef94d198e1f05e6fb8
Stephen Garver <sgarver@protonmail.ch>
2015-11-13 07:45:12 -0800

    Add const filename

--- rotu.js
+++ rotu.js
@@ -7,17 +7,18 @@ module.exports = function(route, data, options, err) {
 
     try {
 
+        options = options || {};
+
         var routed = path.normalize(url.parse(route).pathname);
 
         if (routed[routed.length - 1] === '/') {
             routed += "index";
         }
 
-        options = options || {};
+        const filename = path.basename(routed);
+        const template = jade.compileFile('./pages' + routed + '.jade', options);
 
-        var template = jade.compileFile('./pages' + routed + '.jade', options);
-
-        return template(data[path.basename(routed)].locals);
+        return template(data[filename].locals);
 
     } catch(e) {
         typeof err === 'function' && err(e);
