4505785f92d8ea337658124f3a4b7c8dc7195c4d
Stephen Garver <sgarver@protonmail.ch>
2016-01-02 08:21:06 -0800

    Add support for 404 template

--- rotu.js
+++ rotu.js
@@ -11,20 +11,21 @@ module.exports = (function() {
             "root": ".",
             "data": null,
             "options": {"pretty": true},
-            "error": null
+            "error": null,
+            "missing": "./404"
         },
         "route": function() {
 
             this.routed = path.normalize(url.parse(this.config.route).pathname);
 
             var endpoint = this.routed[this.routed.length - 1];
-            console.log("rotu >>> " + this.routed);
 
             if (endpoint === "/" || endpoint === "\\") {
                 this.routed += "index";
             }
 
             return this.routed;
+
         },
         "html": function() {
 
@@ -35,6 +36,8 @@ module.exports = (function() {
                 var filename = path.basename(this.routed);
                 var template = jade.compileFile(this.config.root + this.routed + ".jade", this.config.options);
 
+                console.log("rotu >>> " + this.routed);
+
                 if (this.config.data) {
                     return template(this.config.data[filename].locals);
                 } else {
@@ -42,7 +45,16 @@ module.exports = (function() {
                 }
 
             } catch (ex) {
+
+                if (ex.code === "ENOENT") {
+                    var template = jade.compileFile(this.config.missing + ".jade", this.config.options);
+
+                    console.log("template missing >>> " + this.routed);
+                    return template();
+                }
+
                 typeof this.config.error === "function" && this.config.error(ex);
+
             }
         }
     };
