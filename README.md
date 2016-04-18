1130f67b809d117d0507c793ca7d817b035be3da
Stephen Garver <sgarver@protonmail.com>
2016-04-17 21:45:10 -0700

    Release 1.1.4 - Upgrade to ES6 class pattern

--- README.md
+++ README.md
@@ -5,19 +5,24 @@ _micro page routing for [JADE](http://jade-lang.com)_
 **v1.1.4**
 
 ```javascript
-var http = require("http");
-var rotu = require("rotu");
 
-var server = http.createServer(function (req, res) {
+"use strict";
+
+const http = require("http");
+const rotu = require("rotu");
+
+let server = http.createServer(function (req, res) {
+
+    const r = new rotu();
 
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.end(rotu.html());
+    res.end(r.html());
 });
 
 server.listen(8000);
 ```
 
-See [rotu boilerplate](https://github.com/sgarver/rotu-boilerplate) for example project.
+See [rotu boilerplate](https://github.com/sgarver/rotu-boilerplate) for an example project.
 
 The above _micro_ [Node.js&reg;](https://nodejs.org) application will respond to the request for [http://localhost:8000](http://localhost:8000) by looking in the current directory for a jade template named index.jade, compile the template and then serve the resultant html.
 
@@ -39,8 +44,8 @@ $ npm install rotu
 
 # configuration
 
-rotu.**config** | _object_  
-An options object with following default values:  
+let r = new rotu(**config**) | _object_  
+An _optional_ configuration object that can be passed as an argument to the rotu constructor with following default values:  
 ```javascript
 {
     "route": "/",
@@ -57,25 +62,25 @@ An options object with following default values:
 
 
 
-rotu.config.**route** | _string_  
+config.**route** | _string_  
 The url returned from the http.createServer() request object.
 
 ```javascript
-rotu.config.route = req.url;
+config.route = req.url;
 ```
 
-rotu.config.**root** | _string_  
+config.**root** | _string_  
 The root path used to override the default which is the _current working directory_.
 
 ```javascript
-rotu.config.root = "./pages";
+config.root = "./pages";
 ```
 
-rotu.config.**data** | _json_  
+config.**data** | _json_  
 The first child of data must be the jade _template name_ (without the .jade extension). This object requires an additional child object named _locals_ containing the data to be bound in the jade template.
 
 ```javascript
-rotu.config.data = {
+config.data = {
     "index": {
         "locals": {
             "pageTitle": "Welcome to this Website",
@@ -91,21 +96,21 @@ rotu.config.data = {
 };
 ```
 
-rotu.config.**options** | _object_  
+config.**options** | _object_  
 The standard jade configuration object.  
 ref: http://jade-lang.com/api/
 
 ```javascript
-rotu.config.options = {
+config.options = {
     "pretty": true,
     "debug": true
 };
 ```
-rotu.config.**error**(exception) | _callback method_  
+config.**error**(exception) | _callback method_  
 A callback method that gets called if an exception is thrown during routing or template compilation.
 
 ```javascript
-rotu.config.error = function(ex) {
+config.error = function(ex) {
     response.end("There was a problem processing your template");
     console.log(ex);
 };
--- rotu.js
+++ rotu.js
@@ -1,61 +1,70 @@
-var path = require("path");
-var url = require("url");
-var jade = require("jade");
 
-module.exports = (function() {
+"use strict";
 
-    return {
-        "config": {
+const path = require("path");
+const url = require("url");
+const jade = require("jade")
+
+module.exports = class rotu {
+
+    constructor(config) {
+
+        this.config = {
             "route": "/",
             "routed": null,
             "root": ".",
             "data": null,
-            "options": {"pretty": true},
+            "options": { "pretty": true },
             "error": null,
             "missing": "./404"
-        },
-        "route": function() {
+        };
 
-            this.routed = path.normalize(url.parse(this.config.route).pathname);
-
-            var endpoint = this.routed[this.routed.length - 1];
-
-            if (endpoint === "/" || endpoint === "\\") {
-                this.routed += "index";
-            }
-
-            return this.routed;
-
-        },
-        "html": function() {
-
-            try {
-
-                this.route();
-
-                var filename = path.basename(this.routed);
-                var template = jade.compileFile(this.config.root + this.routed + ".jade", this.config.options);
-
-                console.log("rotu >>> " + this.routed);
-
-                if (this.config.data) {
-                    return template(this.config.data[filename].locals);
-                } else {
-                    return template();
-                }
-
-            } catch (ex) {
-
-                if (ex.code === "ENOENT") {
-                    var template = jade.compileFile(this.config.missing + ".jade", this.config.options);
-
-                    console.log("template missing >>> " + this.routed);
-                    return template();
-                }
-
-                typeof this.config.error === "function" && this.config.error(ex);
-
-            }
+        if (config) {
+            console.log("config is defined?");
+            Object.assign(this.config, config);
         }
-    };
-})();
+    }
+    route() {
+
+        let routed = path.normalize(url.parse(this.config.route).pathname);
+
+        const endpoint = routed[routed.length - 1];
+
+        if (endpoint === "/" || endpoint === "\\") {
+            routed += "index";
+        }
+
+        this.config.routed =  routed;
+    }
+    html() {
+
+        try {
+
+            this.route();
+
+            const filename = path.basename(this.config.routed);
+            const template = jade.compileFile(this.config.root + this.config.routed + ".jade", this.config.options);
+
+            console.log("rotu >>> " + this.config.routed);
+
+            if (this.config.data) {
+                return template(this.config.data[filename].locals);
+            } else {
+                return template();
+            }
+
+        } catch (ex) {
+
+            if (ex.code === "ENOENT") {
+
+                const template = jade.compileFile(this.config.missing + ".jade", this.config.options);
+
+                console.log("template missing >>> " + this.config.routed);
+
+                return template();
+            }
+
+            typeof this.config.error === "function" && this.config.error(ex);
+        }
+    }
+}
